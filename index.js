571bec7fa05c173bcb7852bcdcd725993f9f14d4
HASANUL BANNA C P <cphasanulbanna@gmail.com>
2023-05-22 02:34:07 +0530

    data fetch function added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const jsdom = require("jsdom");
+const axios = require("axios");
+
 const { JSDOM } = jsdom;
 const app = express();
 
 const PORT = 3005;
+
+const URL = "https://www.amazon.in/s?k=iphone+14+pro+max&ref=nb_sb_ss_ts-doa-p_3_6";
+async function fetchData() {
+    const resposne = await axios(URL);
+    console.log(resposne);
+}
+
+fetchData();
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
